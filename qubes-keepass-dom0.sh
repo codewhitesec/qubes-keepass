f44610026af44c8233f6bb931be1da95b94b9766
Tobias Neitzel <secure@tneitzel.eu>
2022-12-14 08:44:24 +0100

    Add trigger for trust-level
    Added the --trust-level parameter to the trigger script
    qubes-keepass-dom0.sh.

--- qubes-keepass-dom0.sh
+++ qubes-keepass-dom0.sh
@@ -1,33 +1,76 @@
 #!/usr/bin/bash
 
-VAULT="vault"
+
+VAULT='vault'
+
 
 function get_id() {
     # Get the id of the currently focused window. This function
     # was copied from the default resources that are shipped with
-    # Qubes.
-    local id=$(xprop -root _NET_ACTIVE_WINDOW)
-    echo ${id##* }
+    # Qubes OS.
+    #
+    # Parameters:
+    #   None
+    #
+    # Returns:
+    #   currently focused window id
+    #
+    local ID=$(xprop -root _NET_ACTIVE_WINDOW)
+    echo ${ID##* }
 }
 
-function get_vm() {
-    # Get the name of the VM the currently focused window belongs to.
-    # This function was copied from the default resources that are
-    # shipped with Qubes.
-    local id=$(get_id)
-    local vm=$(xprop -id $id | grep '_QUBES_VMNAME(STRING)')
-    local vm=${vm#*\"} # extract vmname
-    echo ${vm%\"*} # extract vmname
-}
 
-function main() {
-    # Get the currently focused VM name and run qubes-keepass.py within
-    # the ${VAULT} VM using the currently focused VM name as argument.
-    local vm=$(get_vm)
+function get_xprop() {
+    # Get the specified xproperty from the specified window ID.
+    #
+    # Parameters:
+    #   ID          the window ID to obtain the property from
+    #   PROP        the property to obtain
+    #   TYPE        the type to obtain (str, int)
+    #
+    # Returns:
+    #   the requested property
+    #
+    local ID=${1}
+    local PROP=${2}
+    local TYPE=${3}
 
-    if [[ -n "${vm}" ]]; then
-        qvm-run ${VAULT} "/home/user/.local/bin/qubes-keepass.py ${vm}"
+    local VALUE=$(xprop -id "${ID}" | grep "${PROP}")
+
+    if [ "${TYPE}" == "str" ]; then
+        VALUE=${VALUE#*\"}
+        echo ${VALUE%\"*}
+
+    elif [ "${TYPE}" == "int" ]; then
+        echo ${VALUE#*= }
+
+    else
+        echo ''
     fi
 }
+
+
+function main() {
+    # Obtain the required properties from the currently focused window and call
+    # qubes-keepass with them.
+    #
+    # Parameters:
+    #   None
+    #
+    # Returns:
+    #   None
+    #
+    set -e
+    set -x
+
+    local ID=$(get_id)
+    local VM=$(get_xprop "${ID}" '_QUBES_VMNAME(STRING)' 'str')
+    local LABEL=$(get_xprop "${ID}" '_QUBES_LABEL(CARDINAL)' 'int')
+
+    if [ -n "${VM}" ] && [ -n "${LABEL}" ]; then
+        qvm-run --no-shell "${VAULT}" '/home/user/.local/bin/qubes-keepass.py' "${VM}" --trust-level "${LABEL}"
+    fi
+}
+
 
 main
